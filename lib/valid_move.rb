204447e37384a9ffa9e8ce0d891b025a11af0694
Jacob Levitin <jlevitin92@gmail.com>
2020-09-07 22:50:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,6 +11,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken(board, index)
+def position_taken?(board, index)
 
 end
